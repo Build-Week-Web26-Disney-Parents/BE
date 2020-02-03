a42dfc749ce1f0f2f00fb5abb158afe8da250a11
josh Hill <Josh_hill15@me.com>
2020-02-03 17:59:22 -0500

    change back

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-require("dotenv").config()
+// require("dotenv").config()
 module.exports = {
 
   development: {
@@ -39,20 +39,20 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL,
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: process.env.DATABASE_URL,
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
